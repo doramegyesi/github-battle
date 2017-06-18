72c3fdcd0923531e93edf7846d2d140729727324
Dora Megyesi <dora.megyesi@gmail.com>
2017-06-18 17:07:43 +0200

    explicit binding

--- this.js
+++ this.js
@@ -29,4 +29,30 @@ var dori = Person('Dori', 31);
 dori.sayName();
 dori.mother.sayName();
 
-//explicit binding
+//explicit binding w/ call()
+var sayName = function(lang1, lang2) {
+    console.log('My name is ' + this.name + ' and I know ' + lang1 + ' and ' + lang2);
+}
+
+var dori = {
+    name: 'Dori',
+    age: 31
+};
+
+var languages = ['JavaScript', 'Python', 'Java'];
+
+sayName.call(dori, languages[0], languages[1]);
+
+//explicit binding w/ apply()
+var sayName = function(lang1, lang2) {
+    console.log('My name is ' + this.name + ' and I know ' + lang1 + ' and ' + lang2);
+}
+
+var dori = {
+    name: 'Dori',
+    age: 31
+};
+
+var languages = ['JavaScript', 'Python', 'Java'];
+
+sayName.apply(dori, languages);
