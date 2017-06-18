8d5219824da59283b5a25ba17a37a58386873af5
Dora Megyesi <dora.megyesi@gmail.com>
2017-06-18 17:26:42 +0200

    getting started with components

---
+++ app/components/App.js
@@ -0,0 +1,12 @@
+var React = require('react');
+
+//component definition
+class App extends React.Component {
+    render() {
+        return(
+            <div>Hello World!</div>
+        )
+    }
+}
+
+module.exports = App;
--- app/index.js
+++ app/index.js
@@ -1,15 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 require('./index.css');
-
-//component definition
-class App extends React.Component {
-    render() {
-        return(
-            <div>Hello World!</div>
-        )
-    }
-}
+var App = require('./components/App.js');
 
 ReactDOM.render(
     <App />,
